e9893aca11e160cd8ab5c72e4761f0a5ad287aae
Kshitij131 <1ms23cy032@msrit.edu>
2025-04-05 12:28:26 +0530

    Completed task 10

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 
+
+function debounce(func, delay) {
+    let timer;
+    return function (...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            func(...args);
+        }, delay);
+    };
+}
+
+function throttle(func, interval) {
+    let lastExecuted = 0;
+    return function (...args) {
+        const now = Date.now();
+        if (now - lastExecuted >= interval) {
+            func(...args);
+            lastExecuted = now;
+        }
+    };
+}
+
+
+const args = process.argv.slice(2);
+const type = args[0];               
+const message = args[1];            
+const delay = parseInt(args[2]);    
+
+if (!type || !message || isNaN(delay)) {
+    console.log("Usage:");
+    console.log('  node index.js debounce "Message" 1000');
+    console.log('  node index.js throttle "Message" 1000');
+    process.exit(1);
+}
+
+if (type === "debounce") {
+    const debouncedFn = debounce((msg) => {
+        console.log(`Debounced: ${msg}`);
+    }, 1000); // Fixed internal delay
+
+    setTimeout(() => {
+        debouncedFn(message);
+    }, delay);
+}
+
+else if (type === "throttle") {
+    const throttledFn = throttle((msg) => {
+        console.log(`Throttled: ${msg}`);
+    }, 1000); // Fixed internal interval
+
+    setTimeout(() => {
+        throttledFn(message);
+    }, delay);
+}
+
+else {
+    console.log("Invalid type. Use 'debounce' or 'throttle'.");
+}
